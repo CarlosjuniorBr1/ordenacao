829da17f7ef87047cf05003533051cfff681c2df
Carlos Junior <66750810+CarlosjuniorBr1@users.noreply.github.com>
2023-06-07 07:36:28 -0300

    Create bubble.c

---
+++ bubble.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+void bubbleSort(int array[], int size) {
+    int i, j, temp;
+    for (i = 0; i < size - 1; i++) {
+        for (j = 0; j < size - i - 1; j++) {
+            if (array[j] > array[j + 1]) {
+                // Troca os elementos adjacentes se estiverem fora de ordem
+                temp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = temp;
+            }
+        }
+    }
+}
+
+int main() {
+    int array[] = {64, 34, 25, 12, 22, 11, 90};
+    int size = sizeof(array) / sizeof(array[0]);
+    
+    printf("Array antes da ordenação:\n");
+    for (int i = 0; i < size; i++) {
+        printf("%d ", array[i]);
+    }
+    
+    bubbleSort(array, size);
+    
+    printf("\nArray depois da ordenação:\n");
+    for (int i = 0; i < size; i++) {
+        printf("%d ", array[i]);
+    }
+    
+    return 0;
+}
